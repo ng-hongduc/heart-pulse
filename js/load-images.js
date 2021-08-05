a1e8845a38fb33ac37eac9040a4f93818ad3c148
ng-hongduc <ng.hongduc05@gmail.com>
2021-08-05 21:00:00 +0700

    feat: integrate GitHub Gist for dynamic images and remove local links.txt

--- js/load-images.js
+++ js/load-images.js
@@ -1,59 +1,53 @@
-// load-images.js
+// js/load-images.js
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("%c[Git-Project] 1. Initialization: HTML structure successfully loaded by browser.", "color: #007bff; font-weight: bold;");
+    console.log("%c[Git-Project] 1. Initialization: HTML structure successfully loaded.", "color: #007bff; font-weight: bold;");
     
     const spinContainer = document.getElementById("spin-container");
-    
-    // Check if the target image container exists in the HTML DOM
     if (!spinContainer) {
         console.error("[Git-Project] ERROR: <div id='spin-container'> not found in the HTML file!");
         return;
     }
     console.log("[Git-Project] Target #spin-container element located successfully.");
 
-    console.log("[Git-Project] 2. Fetching links.txt file...");
     
-    // Read content from the links.txt file
-    fetch('./links.txt')
+    const gistRawUrl = "https://gist.githubusercontent.com/ng-hongduc/1a03cb278384ebe3dc42994fd4e082a7/raw/c037f98fac2c13865cda0b209f3955a3401a41f5/links.json";
+
+    const injectImages = (links) => {
+        links.forEach((src, index) => {
+            const img = document.createElement("img");
+            img.src = src;
+            img.alt = `Project Image ${index + 1}`;
+            spinContainer.appendChild(img);
+            console.log(` -> Created <img> element for image #${index + 1}: ${src}`);
+        });
+        
+        console.log(`%c[Git-Project] 3. Success: Injected ${links.length} images into the DOM.`, "color: #28a745; font-weight: bold;");
+        
+        console.log("[Git-Project] 4. Dispatching 'imagesLoaded' event to app.js...");
+        const event = new Event('imagesLoaded');
+        document.dispatchEvent(event);
+    };
+
+    console.log("[Git-Project] 2. Fetching image configuration from GitHub Gist...");
+    
+    fetch(gistRawUrl)
         .then(response => {
-            // Check if the file exists or if it is blocked by CORS policy
             if (!response.ok) {
-                throw new Error(`Failed to load links.txt (Status code: ${response.status})`);
+                throw new Error(`Failed to fetch from Gist (Status code: ${response.status})`);
             }
-            return response.text();
+            return response.json(); 
         })
-        .then(data => {
-            console.log("[Git-Project] Successfully read links.txt. Parsing image URLs...");
-            
-            // Split text lines into a clean array of image URLs
-            const links = data.split('\n')
-                              .map(line => line.trim())
-                              .filter(line => line !== "");
-            
-            console.log(`[Git-Project] Found ${links.length} image link(s) in the txt file:`, links);
-
-            if (links.length === 0) {
-                console.warn("[Git-Project] WARNING: links.txt file appears to be empty!");
+        .then(links => {
+            console.log("[Git-Project] Successfully fetched and parsed JSON from Gist:", links);
+            if (!Array.isArray(links) || links.length === 0) {
+                console.warn("[Git-Project] WARNING: JSON format is empty! Falling back to local images.");
+                injectImages(defaultImages);
+            } else {
+                injectImages(links);
             }
-
-            // 3. Dynamically create and append <img> tags into spin-container
-            links.forEach((src, index) => {
-                const img = document.createElement("img");
-                img.src = src;
-                img.alt = `Project Image ${index + 1}`;
-                spinContainer.appendChild(img);
-                console.log(` -> Created <img> element for image #${index + 1}: ${src}`);
-            });
-
-            console.log(`%c[Git-Project] 3. Success: Injected ${links.length} images into the DOM.`, "color: #28a745; font-weight: bold;");
-
-            // 4. Dispatch a custom event to notify app.js that images are ready
-            console.log("[Git-Project] 4. Dispatching 'imagesLoaded' event to app.js...");
-            const event = new Event('imagesLoaded');
-            document.dispatchEvent(event);
         })
         .catch(err => {
-            console.error("%c[Git-Project] SYSTEM ERROR:", "color: #dc3545; font-weight: bold;", err.message);
-            console.error("[Troubleshooting] Are you opening the HTML file directly via double-click? Please run the project using VS Code Live Server or host it on GitHub Pages to avoid CORS security restrictions when fetching local text files.");
+            console.warn(`%c[Git-Project] Gist fetch failed: ${err.message}. Switching to local backup images.`, "color: #ffc107;");
+            injectImages(defaultImages);
         });
 });
